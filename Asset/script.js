a7edc912629fd5777af303eb7da378524426ad5d
Jayin <jayinxxc@gmail.com>
2019-09-29 07:46:49 -0400

    name changes

--- Asset/script.js
+++ Asset/script.js
@@ -1,19 +1,17 @@
-var j = 0; // to keep track of how many times you want to loop, you have to simulate a for loop:
+var j = 0; 
 
 var myAppendingFunction = function(){
 
 	if(j % 10 === 3){
 	document.getElementById("root_5_setinterval").insertAdjacentHTML("beforeend", ` 
-		<div class="heartblue" style="
-		"><img class="hpng"src="Asset/hpng_2@300x.png"></div>`);
+		<div class="heartblue"><img class="hpng"src="Asset/hpng_2@300x.png"></div>`);
 }else{
 	document.getElementById("root_5_setinterval").insertAdjacentHTML("beforeend", ` 
-		<div class="heartred" style=""><img class="hpng"src="Asset/hpng@300x.png"></div>`);
+		<div class="heartred"><img class="hpng"src="Asset/hpng@300x.png"></div>`);
 }
 
 
-	if(j >= 41){ // defining the maximum amount of cycles
-		console.log("we're done!");
+	if(j >= 41){ 
 		clearInterval(myAnimating);
 	}
 
